3fe3a554ed58423f89f935206a059181581e9de7
gxg <498868779@qq.com>
2019-08-11 22:17:02 +0800

    修改dubbo用例1

--- dubbo-consumer/src/main/java/com/xyz/dubbo/consumer/BizService.java
+++ dubbo-consumer/src/main/java/com/xyz/dubbo/consumer/BizService.java
@@ -4,17 +4,17 @@ import com.xyz.dubbo.provider.model.UserVO;
 import com.xyz.dubbo.provider.service.UserService;
 import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BizService {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationContext context = MySpringContext.getAppContext();
         UserService userService = context.getBean(UserService.class); // 获取远程服务代理
         // 隐式传参，必须在调用之前设置
         RpcContext.getContext().setAttachment("index","abc");
         UserVO userVO = userService.queryUserVoById("10002");
         System.out.println(userVO.toString()); // 显示调用结果
+        System.in.read(); // 按任意键退出
 
     }
 }
